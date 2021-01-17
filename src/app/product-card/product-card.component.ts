a676e9a74e55b4fb3ac1f6b4d9c94c0d84846d58
shubham.snu@gmail.com <shubham.snu@gmail.com>
2021-01-18 00:34:21 +0530

    feat: product card added

---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from '../shared/model/product.model';
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent implements OnInit {
+
+  @Input() productDetails: Product;
+  finalPrice: number;
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.finalPrice = this.productDetails.price -
+      (0.01 * this.productDetails.discountPercent * this.productDetails.price);
+  }
+
+  productCardClicked(): void {
+    this.router.navigate(['product']);
+  }
+}
