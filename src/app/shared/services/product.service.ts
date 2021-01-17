b5683dcf090f3df4668542502be1ff597122ea90
shubham.snu@gmail.com <shubham.snu@gmail.com>
2021-01-18 00:34:46 +0530

    feat: mock service added

---
+++ src/app/shared/services/product.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>('../assets/productlist.json');
+  }
+}
